ca91c1ea9328e40025a4752105b951fabca78a64
Aaron Ritter <git@ritter.world>
2024-12-12 20:17:38 +0100

    build(deps): bump nextjs to 15 (#11)
    * build(deps): bump nextjs to 15
    
    * refactor(authOptions): move authOptions in a own util
    
    - https://stackoverflow.com/questions/78876478/route-src-app-api-auth-nextauth-route-ts-does-not-match-the-required-type

--- complete-application/src/app/makechange/page.tsx
+++ complete-application/src/app/makechange/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from "@/app/utils/authOptions";
 import { redirect } from 'next/navigation';
 import MakeChangeForm from '../../components/MakeChangeForm';
 
